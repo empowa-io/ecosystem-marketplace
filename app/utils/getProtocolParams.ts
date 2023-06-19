d8343f06110859f77db4898279779b5601be0db5
michele-nuzzi <michele.nuzzi.2014@gamil.com>
2023-06-19 20:08:45 +0200

    cache protocol params

--- app/utils/getProtocolParams.ts
+++ app/utils/getProtocolParams.ts
@@ -12,41 +12,51 @@ const env = cfg.envFolderPath;
 
 const ppPath = `${env}/protocol_params.json`;
 
+let _pps: ProtocolParamters | undefined = undefined;
+
+function ppsFromJsonString( jsonStr: string ): ProtocolParamters
+{
+    return JSON.parse(
+        jsonStr,
+        ( k, v ) =>  {
+            if( typeof v === "string" )
+            {
+                try{
+                    return BigInt( v )
+                }
+                finally {
+                    return v;
+                }
+            }
+
+            return v;
+        }
+    ) 
+}
+
 export async function getProtocolParams(): Promise<ProtocolParamters>
 {
+    if( _pps ) return _pps;
+    
     if( !existsSync( ppPath ) )
     {
-        return JSON.parse(
-            await readFile( ppPath, { encoding: "utf-8"} ),
-            ( k, v ) =>  {
-                if( typeof v === "string" )
-                {
-                    try{
-                        return BigInt( v )
-                    }
-                    finally {
-                        return v;
-                    }
-                }
-
-                return v;
-            }
-        ) 
+        return ppsFromJsonString( await readFile( ppPath, { encoding: "utf-8"} ) );
     }
-    const pps = provider instanceof BlockfrostPluts ? await provider.getProtocolParameters() : await koios.epoch.protocolParams();
+    
+    _pps = provider instanceof BlockfrostPluts ? await provider.getProtocolParameters() : await koios.epoch.protocolParams();
 
     await writeFile(
         ppPath,
         JSON.stringify(
-            pps,
+            _pps,
             (k,v) => {
-
-                if( typeof v == "bigint") return v.toString();
+    
+                if( typeof v === "bigint" ) return v.toString();
                 return v;
             }
         ), 
         { encoding: "utf-8" }
     );
-
-    return pps;
+    
+    return _pps;
 }
